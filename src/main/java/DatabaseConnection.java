bab998b64b504b151b35b6482d97b2eaaa6804fb
nelly1411 <nellyderulo@gmail.com>
2026-08-30 13:14:50 +0200

    Create DatabaseConnection.java

---
+++ src/main/java/DatabaseConnection.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    public static Connection connect() throws SQLException {
+        String url = System.getenv("DB_URL");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        return DriverManager.getConnection(url, user, password);
+    }
+}
